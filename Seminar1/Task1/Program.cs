8f5d2ecaa656f7dfb392c45642372c03e0366827
Alexandrych <vyrupaev.00@gmail.com>
2023-07-18 23:13:58 +1000

    Этот коммит просто сохранет результаты

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// 1. Надо попросить ввести число
+// 2. Записать число в переменную
+// 3. Перемножить 2 числа и записать в новую переменную
+// 4. Вывести результат
+Console.Write("Введи число: ");
+
+double number = double.Parse(Console.ReadLine()!);
+double result = number * number;
+Console.WriteLine("Квадрат числа вашего числа равен: " + result);

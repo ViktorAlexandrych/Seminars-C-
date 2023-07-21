55956e6434d34e0b512bbd19656bd1b9c3300780
Alexandrych <vyrupaev.00@gmail.com>
2023-07-21 13:54:45 +1000

    This commit save 1 and 2 seminar

--- Seminar1/Task3/Program.cs
+++ Seminar1/Task3/Program.cs
@@ -2,7 +2,7 @@
 // 3 -> Среда
 // 5 -> Пятница
 System.Console.Write("Введите номер дня недели: ");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine()!);
 if (number == 1) Console.WriteLine("Сегодня понедельник");
 if (number == 2) Console.WriteLine("Сегодня вторник");
 if (number == 3) Console.WriteLine("Сегодня среда");
